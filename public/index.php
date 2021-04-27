5e91807270451d895bd8f66e00ec735a8bc3bf91
sudipto <sudiptocsi@gmail.com>
2021-04-27 19:51:09 +0600

    updated

--- public/index.php
+++ public/index.php
@@ -10,6 +10,7 @@ header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
+
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
 
@@ -33,11 +34,22 @@ $requestMethod = $_SERVER["REQUEST_METHOD"];
 if($uri[1] == 'product'){
     $controller = new ProductController();
     $controller->index();
-    //$controller->processRequest();
 } 
 elseif($uri[1] == 'user' && $uri[2] == 'signup'){
     $controller = new AuthController($dbConnection);
     $controller->signup();
+}
+elseif($uri[1] == 'user' && $uri[2] == 'login'){
+    $controller = new AuthController($dbConnection);
+    $controller->login();
+}
+elseif($uri[1] == 'user' && $uri[2] == 'auth'){
+    $controller = new AuthController($dbConnection);
+    $controller->auth_check();
+}
+elseif($uri[1] == 'user' && $uri[2] == 'token_verify'){
+    $controller = new AuthController($dbConnection);
+    $controller->token_verify();
 }
 else{
     $controller = new PersonController($dbConnection, $requestMethod, $userId);
--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -1,6 +1,8 @@
 <?php
 namespace Src\Controller;
-use \Firebase\JWT\JWT;
+
+use Firebase\JWT\JWT;
+use PDO;
 
 class AuthController
 {
@@ -13,67 +15,173 @@ class AuthController
 
     public function login()
     {
+        // header("Access-Control-Allow-Origin: *");
+        // header("Content-Type: application/json; charset=UTF-8");
+        // header("Access-Control-Allow-Methods: POST");
+        // header("Access-Control-Max-Age: 3600");
+        // header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
+        $email = $_REQUEST['email'];
+        $password = $_REQUEST['password'];
+        $table_name = 'users';
+
+        $query = "SELECT id, firstname, lastname, password FROM " . $table_name . " WHERE email = ? LIMIT 0,1";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindParam(1, $email);
+        $stmt->execute();
+        $num = $stmt->rowCount();
+
+        if ($num > 0) {
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            $id = $row['id'];
+            $firstname = $row['firstname'];
+            $lastname = $row['lastname'];
+            $password2 = $row['password'];
+
+            if (password_verify($password, $password2)) {
+                $secret_key = "WEDEVS";
+                $issuer_claim = "THE_ISSUER"; // this can be the servername
+                $audience_claim = "THE_AUDIENCE";
+                $issuedat_claim = time(); // issued at
+                $notbefore_claim = $issuedat_claim + 10; //not before in seconds
+                $expire_claim = $issuedat_claim + 600000; // expire time in seconds
+                $token = array(
+                    "iss" => $issuer_claim,
+                    "aud" => $audience_claim,
+                    "iat" => $issuedat_claim,
+                    "nbf" => $notbefore_claim,
+                    "exp" => $expire_claim,
+                    "data" => array(
+                        "id" => $id,
+                        "firstname" => $firstname,
+                        "lastname" => $lastname,
+                        "email" => $email,
+                    ));
+                http_response_code(200);
+                $jwt = JWT::encode($token, $secret_key);
+                echo json_encode(
+                    array(
+                        "message" => "Successful login.",
+                        "jwt" => $jwt,
+                        "email" => $email,
+                        "expireAt" => $expire_claim,
+                    ));
+            } else {
+                echo json_encode([
+                    'status' => 'denied',
+                ]);
+            }
+        }
     }
 
     public function signup()
     {
-        // echo json_encode([
-        //     'firstname' => $_POST['firstname'],
-        //     'lastname' => $_POST['lastname']
-        // ]);
-        // http_response_code(200);
-        $firstname = '';
-        $lastname = '';
-        $email = '';
-        $password = '';
-        $conn = null;
-
-        //$databaseService = new DatabaseService();
-        $conn = $this->db;
-
-        /*
-        $data = json_decode(file_get_contents("php://input"));
-        $firstname = $data->firstname;
-        $lastname = $data->lastname;
-        $email = $data->email;
-        $password = $data->password;
-         */
         $firstname = $_REQUEST['firstname'];
         $lastname = $_REQUEST['lastname'];
         $email = $_REQUEST['email'];
         $password = $_REQUEST['password'];
+        $user_type = $_REQUEST['user_type'];
 
-        $table_name = 'Users';
+        $table_name = 'users';
 
         $query = "INSERT INTO " . $table_name . "
-                        SET first_name = :firstname,
-                            last_name = :lastname,
+                        SET firstname = :firstname,
+                            lastname = :lastname,
                             email = :email,
-                            password = :password";
+                            password = :password,
+                            user_type =: user_type";
 
-        $stmt = $conn->prepare($query);
+        $stmt = $this->db->prepare($query);
 
         $stmt->bindParam(':firstname', $firstname);
         $stmt->bindParam(':lastname', $lastname);
         $stmt->bindParam(':email', $email);
-
         $password_hash = password_hash($password, PASSWORD_BCRYPT);
-
         $stmt->bindParam(':password', $password_hash);
+        $stmt->bindParam(':user_type', $user_type);
 
         if ($stmt->execute()) {
             http_response_code(200);
             echo json_encode(array("message" => "User was successfully registered."));
         } else {
             http_response_code(400);
-
             echo json_encode(array("message" => "Unable to register the user."));
         }
     }
 
-    public function logout()
+    public function auth_check()
     {
+        $secret_key = "WEDEVS";
+        $jwt = null;
 
+        $conn = $this->db;
+
+        $data = json_decode(file_get_contents("php://input"));
+
+        $authHeader = $_SERVER['HTTP_AUTHORIZATION'];
+
+        $arr = explode(" ", $authHeader);
+
+        // echo json_encode(array(
+        // "message" => "sd" .$arr[1]
+        // ));
+
+        $jwt = $arr[1];
+
+        if ($jwt) {
+            // echo json_encode([
+            //     'status' => true,
+            // ]);
+            try {
+
+                $decoded = JWT::decode($jwt, $secret_key, array('HS256'));
+                // echo json_encode([
+                //     'status' => true,
+                // ]);
+
+                // Access is granted. Add code of the operation here
+
+                echo json_encode(array(
+                    "message" => "Access granted:",
+                    "auth" => $decoded,
+                    //"error" => $e->getMessage(),
+                ));
+
+            } catch (\Exception $e) {
+
+                http_response_code(401);
+
+                echo json_encode(array(
+                    "message" => "Access denied.",
+                    "error" => $e->getMessage(),
+                ));
+            }
+
+        }
+    }
+
+    public function token_verify()
+    {
+        $secret_key = "WEDEVS";
+        $jwt = null;
+        $authHeader = $_SERVER['HTTP_AUTHORIZATION'];
+        $arr = explode(" ", $authHeader);
+        $jwt = $arr[1];
+        if ($jwt) {
+            try {
+                $decoded = JWT::decode($jwt, $secret_key, array('HS256'));
+                if ($decoded) {
+                    echo json_encode(array(
+                        "status" => true,
+                    ));
+                }
+            } catch (\Exception $e) {
+                http_response_code(401);
+                echo json_encode(array(
+                    "status" => false,
+                    "error" => $e->getMessage(),
+                ));
+            }
+        }
     }
 }
